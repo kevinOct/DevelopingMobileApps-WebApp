ee2a20ffa31f0a3fd5f4cc1bde97e057e48b34af
Annika Grothaus <annika.grothaus@denkbares.com>
2021-11-23 12:54:32 +0100

    added profile page

---
+++ app/src/components/Intro.vue
@@ -0,0 +1,28 @@
+<template>
+    <h2>{{Name}}</h2>
+    <p>{{Status}}</p>
+    <p>{{Interests}}</p>
+</template>
+
+
+
+
+<script>
+    export default{
+        
+        props:{
+            Name: String, 
+            Status: String, 
+            Interests: [], 
+
+        }
+    }
+</script>
+
+
+
+
+
+<style>
+    
+</style>
---
+++ app/src/views/Profile.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="container">
+    <Header />
+    <figure>
+      <img src="../assets/adam.png" alt="">
+    </figure>
+  </div>
+  <div class="text-container">
+    <Intro Name="Adam" Status="Active" Interests="Sports, Nature, Music" />
+
+  <div class="btn-container">
+                  <Button
+                      @btn-click="openProfile()"
+                      text="Connect"
+                      btnClass="primary"
+                    />
+              </div>
+              </div>
+</template>
+
+<script>
+import Header from '../components/Header.vue'
+import Button from '../components/Button.vue'
+import Intro from '../components/Intro.vue'
+
+
+export default {
+  name: 'Profile',
+  components: {
+    Header,
+    Button, 
+    Intro,
+  },
+
+  }
+</script>
+
+<style scoped>
+.text-container{
+     display:flex;
+     flex-direction: column;
+     justify-content: space-between
+
+}
+
+.btn-container {
+  align-self: flex-end;
+}
+</style>
